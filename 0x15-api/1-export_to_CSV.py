b018600c2d6894ab1611cd7bab45894446b2d95b
Tinaguhwa23 <tinaguhwa@gmail.com>
2024-06-03 06:41:25 -0700

    Dictionary of list of dictionaries

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This exports to-do list information for a given employee ID to CSV format."""
+"""Exports to-do list information for a given employee ID to CSV format."""
 import csv
 import requests
 import sys
