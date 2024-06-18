f146fa1b14c5577059a989cb16def1bfe060eb21
Tinaguhwa23 <tinaguhwa@gmail.com>
2024-06-18 05:35:36 -0700

    a recursive function that queries the Reddit API

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -4,7 +4,7 @@ import requests
 
 
 def count_words(subreddit, word_list, instances={}, after="", count=0):
-    """It prints counts of given words found in hot posts of a given subreddit.
+    """Prints counts of given words found in hot posts of a given subreddit.
 
     Args:
         subreddit (str): The subreddit to search.
